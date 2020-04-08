0e04823ff144c00f849a99a839661ad9098abaa8
mourad <chabour.mourad87@gmail.com>
2020-04-08 21:27:49 +0200

    initial server

---
+++ api/order/controllers/order.js
@@ -0,0 +1,43 @@
+'use strict';
+
+/**
+ * Read the documentation (https://strapi.io/documentation/3.0.0-beta.x/concepts/controllers.html#core-controllers)
+ * to customize this controller
+ */
+
+
+const strapi = require('stripe')('sk_test_Xix1Qd20RbMlFI6P334IpQrr');
+console.debug("######### Begin ######");
+console.debug(strapi);
+console.debug("######## END #######");
+
+module.exports = {
+
+
+
+  // create: async ctx => {
+  //
+  //   const {name, total, items, stripeTokenId} = ctx.request.body;
+  //   const {id} = ctx.state.user;
+  //
+  //   const charge = await strapi.charges.create(
+  //     {
+  //       amount: Math.round(total * 100),
+  //       currency: "usd",
+  //       source: stripeTokenId,
+  //       description: `Order ${new Date()} by ${ctx.state.user.username}`
+  //     }
+  //   );
+  //
+  //
+  //   const order =  await  strapi.services.order.create({
+  //     name,
+  //     total,
+  //     items,
+  //     user: id
+  //   });
+  //
+  //   return  order;
+  // }
+
+};
